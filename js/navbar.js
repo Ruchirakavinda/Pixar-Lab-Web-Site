3a3f0ed47486b6c1864808fa0530c05841cf9e83
Ruchirakavinda <ruchirakvnd@gmail.com>
2021-09-24 14:22:29 +0530

    2

---
+++ js/navbar.js
@@ -0,0 +1,14 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar").style.transition="0.5s";
+    
+  } else {
+    document.getElementById("navbar").style.top = "-150px";
+    
+    
+  }
+  prevScrollpos = currentScrollPos;
+}
